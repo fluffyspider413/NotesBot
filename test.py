7124e6c31d54da103e80ac0950fa013bb28a78d5
emilyb25 <90354208+emilyb25@users.noreply.github.com>
2023-07-04 22:46:38 -0400

    copied some github code and idk what it really does but im tired and am going to sleep now

---
+++ test.py
@@ -0,0 +1,6 @@
+testWords = "hello world"
+print(testWords)
+#took me a while to figure out how to comment (i haven't pythoned since the first semester of 8th grade)
+#and ive used java solely for the past 2 years. like a lot of java.
+#but hey it worked
+#look im pythoning
